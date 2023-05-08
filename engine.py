966ef7bf1f6e3f5687b99cca9dc4ffcaddbed575
Aman Jain <amanj120@gmail.com>
2023-05-08 11:26:07 -0400

    more framework code

---
+++ engine.py
@@ -0,0 +1,5 @@
+import hex
+
+# given a hex state, return a float between [-1, 1] representing which player the board state is advantageous for
+def heuristic(state: HexState) -> float:
+    return 0.0
--- hex.py
+++ hex.py
@@ -2,8 +2,8 @@ from typing import List, Set, Tuple, Dict, Optional
 from enum import IntEnum
 
 class Token(IntEnum):
-    A = 1
-    B = 2
+    A = -1
+    B = 1
 
 class HexState():
     def __init__(self, size):
@@ -16,7 +16,6 @@ class HexState():
 
     def __setitem__(self, key: Tuple[int, int], value: Token) -> None:
         self.validateKey(key)
-        assert self[key] is None
         self.board[key[0]][key[1]] = value
     
     def __getitem__(self, key: Tuple[int, int]) -> Optional[Token]:
@@ -57,9 +56,61 @@ class HexState():
         return False
     
     
+class HexGame():
+    def __init__(self, size: int) -> None:
+        assert 5 <= size <= 19, "board size must be between 5 and 19"
+        self.state: HexState = HexState(size)
+        self.moves: List[Tuple[int, int]] = list()
+        self.swapped: bool = False
+        self.curToken: Token = Token.A
+        self.valid: Set[Tuple[int, int]] = set([(i, j) for i in range self.size for j in range self.size])
+    
+    def flipToken(self) -> None:
+        self.curToken = Token.A if self.curToken == Token.B else Token.B
+    
+    def place(self, move: Tuple[int, int]) -> None:
+        assert move in self.valid, "invalid move"
+        self.state[move] = self.curToken
+        self.moves.append(move)
+        self.valid.remove(move)
+        self.flipToken()
+    
+    def undo(self) -> None:
+        if len(self.moves) == 0:
+            return
+        elif len(self.moves) == 1 and self.swapped is True:
+            self.state[self.moves[0]] = self.curToken
+            self.swapped = False
+        else:
+            move = self.moves.pop()
+            del self.state[move]
+            self.valid.add(move)
+        self.flipToken()
+    
+    def swap(self) -> None:
+        assert len(self.moves) == 1 and self.swapped is False, "can only swap after first move"
+        self.swapped = True
+        self.state[self.moves[0]] = self.curToken
+        self.flipToken()
+    
+    def __str__(self) -> str:
+        return str(self.state)
 
 
-state0 = HexState(7)
-state1 = HexState(7)
-state0[(2,3)] = Token.A
-print(state1 < state0)
+if __name__ == "__main__":
+    game = HexGame(7)
+    print(str(game))
+    game.place((2,3))
+    print(str(game))
+    game.swap()
+    print(str(game))
+    game.place((3,2))
+    print(str(game))
+    game.undo()
+    print(str(game))
+    game.undo()
+    print(str(game))
+    game.undo()
+    print(str(game))
+    game.undo()
+    print(str(game))    
